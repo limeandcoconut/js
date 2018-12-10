d8eaca99222c99750f42d9437d7bf91ebbc804b5
Jacob Smith <messagethesmith@gmail.com>
2018-12-09 21:37:45 -0800

    Add gaId

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const frontendPort = 3005
 const siteName = 'Jacob Smith'
 const gaDevId = 'UA-93531536-4'
-const gaProductionId = ''
+const gaProductionId = 'UA-130664559-1'
 
 module.exports = {
     frontendPort,
