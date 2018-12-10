45b2a93bc8fb738f34270a0df6ce4e9adc145856
Jacob Smith <messagethesmith@gmail.com>
2018-12-10 09:23:16 -0800

    Update eject

--- eject.sh
+++ eject.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "Running rm -rf .git"
 rm -rf .git
+
+echo "\nChange: config.js \npublic/manifest.json \nAdd icons \nAuthor in index.template.html \nTheme color in index.template.html \n"
