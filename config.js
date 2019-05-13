38fe8ba649934d411b7d02cdd876119e002c90b8
Jacob Smith <messagethesmith@gmail.com>
2019-05-13 09:55:19 -0700

    swtich ga id

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const frontendPort = 3005
 const siteName = 'Jacob Smith'
-const gaDevId = 'UA-93531536-4'
+const gaDevId = 'UA-137352311-1'
 const gaProductionId = 'UA-130664559-1'
 
 module.exports = {
